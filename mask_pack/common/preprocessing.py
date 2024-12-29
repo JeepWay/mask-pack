eae2073e7b3b336a305174ca4d3512b535bc81a0
JeepWay <william.liu0731@gmail.com>
2024-12-29 15:02:59 +0800

    feat: add normalize observation function

--- mask_pack/common/preprocessing.py
+++ mask_pack/common/preprocessing.py
@@ -4,7 +4,7 @@ from typing import Dict, Tuple, Union
 import numpy as np
 import torch as th
 from gymnasium import spaces
-
+from mask_pack.common.constants import BIN, MASK
 
 def preprocess_obs(
     obs: Union[th.Tensor, Dict[str, th.Tensor]],
@@ -26,15 +26,21 @@ def preprocess_obs(
         # Do not modify by reference the original observation
         assert isinstance(obs, Dict), f"Expected dict, got {type(obs)}"
         preprocessed_obs = {}
-        for key, _obs in obs.items():
-            preprocessed_obs[key] = preprocess_obs(_obs, observation_space[key], normalize_images=normalize_images)
+        preprocessed_obs[BIN] = preprocess_obs(obs[BIN], observation_space[BIN], normalize_images=normalize_images)
+        preprocessed_obs[MASK] = preprocess_obs(obs[MASK], observation_space[MASK], normalize_images=False)
+        # for key, _obs in obs.items():
+        #     preprocessed_obs[key] = preprocess_obs(_obs, observation_space[key], normalize_images=normalize_images)
         return preprocessed_obs  # type: ignore[return-value]
 
     assert isinstance(obs, th.Tensor), f"Expecting a torch Tensor, but got {type(obs)}"
 
     if isinstance(observation_space, spaces.Box):
         if normalize_images:
-            return obs.float() / 255.0
+            _, bin_w, bin_h = observation_space.shape
+            obs = obs.float().clone() 
+            obs[:, 1, :, :] = obs[:, 1, :, :] / bin_w
+            obs[:, 2, :, :] = obs[:, 2, :, :] / bin_h
+            return obs
         return obs.float()
     else:
         raise NotImplementedError(f"Preprocessing not implemented for {observation_space}")
